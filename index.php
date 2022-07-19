d917b68901cada45de1ec73e7dd91e3f8f31f4e4
Laura Daniela <lhaogonzaleza@gmail.com>
2022-07-19 11:49:31 -1000

    Se agrega index.php y composer.js

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv('mysql://b375f2b0e09217:c9cbe69b@us-cdbr-east-06.cleardb.net/heroku_b8e35bcf3ccb2d6?reconnect=true'));
+$cleardb_url = parse_url(getenv('heroku_b8e35bcf3ccb2d6'));
 $cleardb_server = $cleardb_url['us-cdbr-east-06.cleardb.net'];
 $cleardb_username = $cleardb_url['b375f2b0e09217'];
 $cleardb_password = $cleardb_url['c9cbe69b'];
-$cleardb_db = substr($cleardb_url['path'],1);
+$cleardb_db = substr($cleardb_url['mysql://b375f2b0e09217:c9cbe69b@us-cdbr-east-06.cleardb.net/heroku_b8e35bcf3ccb2d6?reconnect=true'],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
